eafed2776414121ee91038bc441b4c2c37f6e5f6
Prakhar Shukla <43924302+prakhar234@users.noreply.github.com>
2021-01-20 22:38:07 +0100

    add production configuration
    add production configuration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -6,5 +7,13 @@ const PORT = process.env.PORT || 5000;
 
 // Define Routes
 app.use('/api/currency', require('./routes/api/currency'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
